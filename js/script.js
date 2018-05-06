b8c6d4a4ff614f8a779df44d3190552eae994d8d
naverstay <naverstay.me@gmail.com>
2018-05-06 19:32:46 +0300

    index

--- js/script.js
+++ js/script.js
@@ -18,8 +18,7 @@ $(function ($) {
 
         all_btns.each(function (ind) {
             var btn = $(this);
-
-            filters[btn.text()] = btn.hasClass('_active');
+            filters[btn.attr('data-filter')] = btn.hasClass('_active');
         });
 
         objectManager.setFilter(getFilterFunction(filters));
@@ -114,11 +113,126 @@ function init() {
         objectManager.add(data);
     });
 
+    objectManager.objects.events.add('balloonopen', function (e) {
+        // Получим объект, на котором открылся балун.
+        var id = e.get('objectId'),
+            geoObject = objectManager.objects.getById(id);
+        // Загрузим данные для объекта при необходимости.
+        downloadContent([geoObject], id);
+    });
+}
+
+function downloadContent(geoObjects, id, isCluster) {
+    // Создадим массив меток, для которых данные ещё не загружены.
+    var array = geoObjects.filter(function (geoObject) {
+            return geoObject.properties.balloonContent === 'идет загрузка...' ||
+                geoObject.properties.balloonContent === 'Not found';
+        }),
+        // Формируем массив идентификаторов, который будет передан серверу.
+        ids = array.map(function (geoObject) {
+            return geoObject.id;
+        });
+    if (ids.length) {
+        // Запрос к серверу.
+        // Сервер обработает массив идентификаторов и на его основе
+        // вернет JSON-объект, содержащий текст балуна для
+        // заданных меток.
+        ymaps.vow.resolve($.ajax({
+            // Обратите внимание, что серверную часть необходимо реализовать самостоятельно.
+            //contentType: 'application/json',
+            //type: 'POST',
+            //data: JSON.stringify(ids),
+            url: id % 2 ? 'list.json' : 'single.json',
+            dataType: 'json',
+            processData: false
+        })).then(function (data) {
+            // Имитируем задержку от сервера.
+            return ymaps.vow.delay(data, 100);
+        }).then(
+            function (data) {
+                console.log(data.balloonContent, data.balloonContent.length);
+                geoObjects.forEach(function (geoObject) {
+                    var balloon_html = '';
+                    // Содержимое балуна берем из данных, полученных от сервера.
+                    // Сервер возвращает массив объектов вида:
+                    // [ {"balloonContent": "Содержимое балуна"}, ...]
+
+                    if (data.balloonContent.length > 1) {
+                        balloon_html = '<dl class="balloon_list">';
+
+                        for (var i = 0; i < data.balloonContent.length; i++) {
+                            balloon_html += balloon_item_builder(data.balloonContent[i]);
+                        }
+
+                        balloon_html += '</dl>';
+
+                    } else {
+                        var item = data.balloonContent[0];
+
+                        balloon_html = '<div class="balloon_content">' +
+                            '<div class="balloon_img"><img src="' + item.object_image_url + '" alt=""></div>' +
+                            '<div class="balloon_name">' + item.object_name + '</div>' +
+                            '<dl class="balloon_features">' +
+                            '<dt>Класс:</dt>' +
+                            '<dd>' + item.object_class + '</dd>' +
+                            '<dt>Цена:</dt>' +
+                            '<dd>' + item.object_price + '</dd>' +
+                                balloon_transport_builder(item.object_transport) +
+                            '</dl>' +
+                            '</div>';
+                    }
+
+                    geoObject.properties.balloonContent = balloon_html;
+                });
+                // Оповещаем балун, что нужно применить новые данные.
+                setNewData();
+            }, function () {
+                geoObjects.forEach(function (geoObject) {
+                    geoObject.properties.balloonContent = 'Not found';
+                });
+                // Оповещаем балун, что нужно применить новые данные.
+                setNewData();
+            }
+        );
+    }
+
+    function setNewData() {
+        if (isCluster && objectManager.clusters.balloon.isOpen(id)) {
+            objectManager.clusters.balloon.setData(objectManager.clusters.balloon.getData());
+        } else if (objectManager.objects.balloon.isOpen(id)) {
+            objectManager.objects.balloon.setData(objectManager.objects.balloon.getData());
+        }
+    }
+}
+
+function balloon_item_builder(item) {
+    return '<dt>' + item.object_name + '</dt>' +
+        '<dd>' + item.object_class + ', ' + item.object_price + '</dd>';
+}
+
+function balloon_transport_builder(transport) {
+    var ret = '';
+
+    for (var i = 0; i < transport.length; i++) {
+        var tr = transport[i];
+
+        ret += '<dt class="_wide">' + tr.transport_label + '</dt>' +
+            '<dd class="_wide"><span>' + tr.transport_name + '</span>'
+            + balloon_transport_icon(tr.transport_type)
+            + '<span>' + tr.transport_time + '</span>' +
+            '</dd>';
+    }
+
+    return ret;
+}
+
+function balloon_transport_icon(icon) {
+    return '<span class="balloon_transport_icon _' + icon + '"></span>'
 }
 
 function getFilterFunction(categories) {
     return function (obj) {
-        var content = obj.properties.balloonContent;
+        var content = obj.properties.tag;
         return categories[content]
     }
 }
